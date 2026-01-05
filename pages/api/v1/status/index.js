b51a5207982f63d2f824ed41fd19554b452f1933
Patrick <patrick.almeida06@gmail.com>
2026-01-04 22:00:34 -0300

    finish pi/v1/status end point

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,27 @@
-import database from "infra/databese.js";
+import database from "infra/database.js";
 export default async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  res.status(200).json({ message: "API is running" });
+  const updateAt = new Date().toISOString();
+
+  const version = await database.query({
+    text: "SHOW server_version;",
+  });
+  const max_conections = await database.query({
+    text: "SHOW max_connections;",
+  });
+  const databaseName = process.env.POSTGRES_DB;
+  const using_conections = await database.query({
+    text: `SELECT COUNT(*)::INT FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const databaseOpenedConnections = parseInt(using_conections.rows[0].count);
+  res.status(200).json({
+    update_At: updateAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_Connections: parseInt(max_conections.rows[0].max_connections),
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
--- tests/integration/api/v1/STATUS/get.test.js
+++ tests/integration/api/v1/STATUS/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody).toHaveProperty("update_At");
+
+  const parsedUpdateAt = new Date(responseBody.update_At).toISOString();
+  expect(responseBody.update_At).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.max_Connections).toEqual(100);
+  expect(
+    responseBody.dependencies.database.opened_connections,
+  ).toBeGreaterThanOrEqual(1);
 });
