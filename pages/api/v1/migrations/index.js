e4162eeab33e1c4e6e31a3aa613ca690469a6d7b
patrick <patrick.almeida06@gmail.com>
2026-02-09 16:30:10 -0300

    fix: 'migrations' endpont connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,18 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
+import { error } from "node:console";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewCliente();
+  const alowedMethods = ["GET", "POST"];
+  let dbClient;
+
+  if (!alowedMethods.includes(req.method)) {
+    return res.status(405).json({
+      error: "Method not allowed",
+    });
+  }
+  dbClient = await database.getNewCliente();
 
   const defaultMigrationOptions = {
     dbClient: dbClient,
@@ -13,34 +22,35 @@ export default async function migrations(req, res) {
     verbose: true,
     migrationsTable: "pgmigrations",
   };
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: true,
-    });
-    await dbClient.end();
-
-    return res.status(200).json(pendingMigrations);
-  }
-  if (req.method === "POST") {
-    try {
-      const migratedMigrations = await migrationRunner({
+  try {
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner({
         ...defaultMigrationOptions,
-        dryRun: false,
+        dryRun: true,
       });
 
-      return res
-        .status(migratedMigrations.length > 0 ? 201 : 200)
-        .json(migratedMigrations);
-    } catch (error) {
-      if (error.message?.includes("Another migration is already running")) {
-        return res.status(200).json([]);
-      }
-      throw error;
-    } finally {
-      await dbClient.end(); // aqui está a correção
+      return res.status(200).json(pendingMigrations);
     }
-  }
+    if (req.method === "POST") {
+      try {
+        const migratedMigrations = await migrationRunner({
+          ...defaultMigrationOptions,
+          dryRun: false,
+        });
 
-  return res.status(405).json({ message: "Method Not Allowed" });
+        return res
+          .status(migratedMigrations.length > 0 ? 201 : 200)
+          .json(migratedMigrations);
+      } catch (error) {
+        if (error.message?.includes("Another migration is already running")) {
+          return res.status(200).json([]);
+        }
+        throw error;
+      }
+    }
+  } catch (error) {
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
