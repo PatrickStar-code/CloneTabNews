30835953c540f642f4bdf72cc1f8fd482642b2b5
anabzvmf <anabetrizzvmf@gmail.com>
2026-02-23 16:12:21 -0300

    makes npm test ´more robust with ´async retry´ and ´orchestrator´

--- tests/integration/api/v1/STATUS/get.test.js
+++ tests/integration/api/v1/STATUS/get.test.js
@@ -12,7 +12,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.update_At).toISOString();
   expect(responseBody.update_At).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.11 (df20cf9)");
+  expect(responseBody.dependencies.database.version).toEqual("16.12 (6d3029c)");
   expect(responseBody.dependencies.database.max_Connections).toEqual(901);
   expect(
     responseBody.dependencies.database.opened_connections,
